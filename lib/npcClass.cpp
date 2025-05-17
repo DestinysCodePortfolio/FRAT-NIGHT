a2b27782aa85ba8f84c62a35ce17c598efb9dd69
DestinysCodePortfolio <151272952+DestinysCodePortfolio@users.noreply.github.com>
2025-05-17 21:42:14 +0000

    commitiing the npcClass cpp

---
+++ lib/npcClass.cpp
@@ -0,0 +1,26 @@
+#include "../header/npcClass.hpp"
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <fstream>
+#include <stdexcept>
+#include <vector>
+
+ void npcClass:: gainTrust(){
+    trustMeter = 1;
+ }
+ void npcClass:: looseTrust(){
+    trustMeter -= 1;
+ }
+void npcClass:: romance(){
+    
+ }
+
+ void npcClass:: agression(){
+   
+ }
+
+ void npcClass:: intoxicated(){
+ }
+
+
