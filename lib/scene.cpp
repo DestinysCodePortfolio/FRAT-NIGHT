0db964d100cd42ffa1cf2f864ab0c16efc8de185
avamwilliams <ava.williamsx2@gmail.com>
2025-05-29 21:23:21 -0700

    added first shot and second shot scenes

--- lib/scene.cpp
+++ lib/scene.cpp
@@ -56,7 +56,7 @@ void openingScene::updatePossibleScenes(vector<Scene*>& nextPossibleScenes){
 //if you take the hug
 void takeHug::dialogue(){
     string output = "";
-    output = " He reeks of tequila and cologne that never fully covered up whatever he did last night.";
+    output = " He reeks of tequila and cologne that never fully covered up whatever he did last night.\n Natalie: Chad, have you been drinking?! \n Chad: Have you not? It's Halloweekend, babe. \n It’s Thursday.";
     trickleDisplayString(output, 50);
 }
 void takeHug::updatePossibleScenes(vector<Scene*>& nextPossibleScenes){
@@ -76,6 +76,65 @@ void rejectHug::updatePossibleScenes(vector<Scene*>& nextPossibleScenes){
 		cout<<currentPointer->getOptionName()<<endl;
 	}
 }
+
+//Continuing scene: kevin and lilith intro
+void firstShotOptionScene::dialogue(){
+    string output = "";
+    output = "Natalie: Whatever. Have you seen [REDACTED]? She was not at her apartment, has not shown up to class, and her location is off… 
+    \n Chad: Bro… I dunno. She is probably out getting hazed with her bigs or whatever. \n His eyes light up. 
+    \nChad: But yo, forget that—you need to catch up. SHOTS. NOW. \n He grabs my arm, dragging me towards the kitchen. 
+    \n Lilith: That girl was so hard to keep still — \n Kevin: NATALIEEE! Youre so late! Lilith already threw up twice! 
+    \n It is 10:15 pm…the party started at 10. \n Lilith: Kevin, that was you. \n Kevin: IT IS FIVE O'CLOCK SOMEWHEREEEE, SO— 
+    \n Lilith: So where is that friend of yours? [REDACTED], right? \n Natalie: I could ask you the same question. Aren't you the sweetheart of this frat? 
+    \n Lilith: I mean, yeah—but we got like, over a hundred girls rushing. I dont remember every newbie.
+    \n Chad walks pulls two bottles out of the fridge, waving them around.
+    \n Chad : PICK YOUR POISON!
+    \n Take the shot[t]
+    \n Reject the shot[r]";
+    trickleDisplayString(output, 50);
+}
+void firstShotOptionScene::updatePossibleScenes(vector<Scene*>& nextPossibleScenes){
+	nextPossibleScenes.resize(2);
+	nextPossibleScenes.at(0)=new takeFirstShot('t');
+	nextPossibleScenes.at(1)=new rejectFirstShot('r');
+	std::cout<<"Updated possible Scenes\n";
+}
+
+//take first shot
+void secondShotOptionScene::dialogue(){
+    string output = "";
+    output = "Kevin: WOOOAAAAAHH THAS LIQA!!!
+    \n Lilith: I'm tired of babysitting him, Chad, tag in.
+    \n Lilith grabs my arm and pulls me to the couch.
+    \n Lilith: So what do you think of Kevin , he's cute huh? 
+    \n Well yes..
+    \n Natalie: Dude…
+    \n Lilith: My bad I'm just trying to help my bro out. After all, I heard that you and your man just broke up..
+    \n She pulls out small bottle of tequila from her purse.
+    \n Lilith: Oops did that slip out , well I'm too deep in now to not tell you, but i need another shot so WE forget that I told you!
+    \n Take the shot: Kevin’s cute, and I wanna know more[t]
+    \n Reject the shot: I need to find my friend[r]";
+    trickleDisplayString(output, 50);
+}
+void secondShotOptionScene::updatePossibleScenes(vector<Scene*>& nextPossibleScenes){
+	nextPossibleScenes.resize(2);
+	nextPossibleScenes.at(0)=new takeSecondShot('t');
+	nextPossibleScenes.at(1)=new rejectSecondShot('r');
+	std::cout<<"Updated possible Scenes\n";
+}
+
+
+//reject first shot 
+void rejectFirstShot::dialogue(){
+    string output = "";
+    output = "Chad: I know you're paranoid about your friend and all , but she aint here little lady. Stop killing the vibe.
+    \n Natalie - Fine I'll find her myself, jerk.";
+    trickleDisplayString(output, 50);
+}
+
+
+//takes second shot
+
 
 void Scene::trickleDisplayString(const string& inputString, uint8_t delay){
     const size_t BOX_WIDTH = 70; // Width inside the borders
