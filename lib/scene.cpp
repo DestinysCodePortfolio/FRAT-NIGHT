6cc2fc22a09b87fdfb7ac3dd7d1382b0e141047b
iqnc06 <iancatrenucr@gmail.com>
2025-05-31 00:22:19 -0700

    Update scene.cpp
    Brought from around 50 errors down to 6. Assuming some errors might be brought on by the while loop condition implementation.

--- lib/scene.cpp
+++ lib/scene.cpp
@@ -21,21 +21,25 @@ using std::cin;
 // }
 
 
+
+
 //openers dialogue-- until where my hug at
 Scene::Scene():
-	optionName(0)
+    optionName(0)
 {}
 Scene::Scene(char name):
-	optionName(name)
+    optionName(name)
 {}
 char Scene::getOptionName(){
-	return optionName;
+    return optionName;
 }
 void openingScene::dialogue(){
     string output = "You are Natalie Fitzgerald, a university student whose friend went missing at a frat party...\n";
     trickleDisplayString(output, 1);
 
+
     // cin >> userChoice;
+
 
     // if (choice == 'a') {
     //     return new takeHug();
@@ -43,97 +47,111 @@ void openingScene::dialogue(){
     //     return new rejectHug();
     // }
 
+
     // return nullptr; // if invalid input
 }
 void openingScene::updatePossibleScenes(vector<Scene*>& nextPossibleScenes){
-	nextPossibleScenes.resize(2);
-	nextPossibleScenes.at(0)=new takeHug('t');
-	nextPossibleScenes.at(1)=new rejectHug('r');
-	std::cout<<"Updated possible Scenes\n";
+    nextPossibleScenes.resize(2);
+    takeHug* hugSymbol = new takeHug('t');
+    nextPossibleScenes.at(0)= hugSymbol;
+    rejectHug* rejectSymbol = new rejectHug('r');
+    nextPossibleScenes.at(1)= rejectSymbol;
+    cout<<"Updated possible Scenes\n";
 }
+
+
 
 
 //if you take the hug
 void takeHug::dialogue(){
-    string output = "";
-    output = " He reeks of tequila and cologne that never fully covered up whatever he did last night.\n Natalie: Chad, have you been drinking?! \n Chad: Have you not? It's Halloweekend, babe. \n It’s Thursday.";
+    string output = " He reeks of tequila and cologne that never fully covered up whatever he did last night.\n Natalie: Chad, have you been drinking?! \n Chad: Have you not? It's Halloweekend, babe. \n It’s Thursday.";
     trickleDisplayString(output, 50);
 }
 void takeHug::updatePossibleScenes(vector<Scene*>& nextPossibleScenes){
-	for(Scene* currentPointer : nextPossibleScenes){
-		cout<<currentPointer->getOptionName()<<endl;
-	}
+    for(Scene* currentPointer : nextPossibleScenes){
+        cout<<currentPointer->getOptionName()<<endl;
+    }
 }
+
 
 //if you dont take the hug
 void rejectHug::dialogue(){
-    string output = "";
-    output = "Damn, Natalie. Acting brand new?";
+    string output = "Damn, Natalie. Acting brand new?";
     trickleDisplayString(output, 50);
 }
 void rejectHug::updatePossibleScenes(vector<Scene*>& nextPossibleScenes){
-	for(Scene* currentPointer : nextPossibleScenes){
-		cout<<currentPointer->getOptionName()<<endl;
-	}
+    for(Scene* currentPointer : nextPossibleScenes){
+        cout<<currentPointer->getOptionName()<<endl;
+    }
 }
+
 
 //Continuing scene: kevin and lilith intro
 void firstShotOptionScene::dialogue(){
-    string output = "";
-    output = "Natalie: Whatever. Have you seen [REDACTED]? She was not at her apartment, has not shown up to class, and her location is off… 
-    \n Chad: Bro… I dunno. She is probably out getting hazed with her bigs or whatever. \n His eyes light up. 
-    \nChad: But yo, forget that—you need to catch up. SHOTS. NOW. \n He grabs my arm, dragging me towards the kitchen. 
-    \n Lilith: That girl was so hard to keep still — \n Kevin: NATALIEEE! Youre so late! Lilith already threw up twice! 
-    \n It is 10:15 pm…the party started at 10. \n Lilith: Kevin, that was you. \n Kevin: IT IS FIVE O'CLOCK SOMEWHEREEEE, SO— 
-    \n Lilith: So where is that friend of yours? [REDACTED], right? \n Natalie: I could ask you the same question. Aren't you the sweetheart of this frat? 
-    \n Lilith: I mean, yeah—but we got like, over a hundred girls rushing. I dont remember every newbie.
-    \n Chad walks pulls two bottles out of the fridge, waving them around.
-    \n Chad : PICK YOUR POISON!
-    \n Take the shot[t]
-    \n Reject the shot[r]";
+    string output = "Natalie: Whatever. Have you seen [REDACTED]? She was not at her apartment, has not shown up to class, and her location is off…" +
+    "\n Chad: Bro… I dunno. She is probably out getting hazed with her bigs or whatever." +
+    "\n His eyes light up. " +
+    "\nChad: But yo, forget that—you need to catch up. SHOTS. NOW. \n He grabs my arm, dragging me towards the kitchen. " +
+    "\n Lilith: That girl was so hard to keep still — \n Kevin: NATALIEEE! Youre so late! Lilith already threw up twice! " +
+    "\n It is 10:15 pm…the party started at 10. \n Lilith: Kevin, that was you. \n Kevin: IT IS FIVE O'CLOCK SOMEWHEREEEE, SO— " +
+    "\n Lilith: So where is that friend of yours? [REDACTED], right? \n Natalie: I could ask you the same question. Aren't you the sweetheart of this frat? " +
+    "\n Lilith: I mean, yeah—but we got like, over a hundred girls rushing. I dont remember every newbie." +
+    "\n Chad walks pulls two bottles out of the fridge, waving them around." +
+    "\n Chad : PICK YOUR POISON!"+
+    "\n Take the shot[t]"+
+    "\n Reject the shot[r]";
     trickleDisplayString(output, 50);
 }
 void firstShotOptionScene::updatePossibleScenes(vector<Scene*>& nextPossibleScenes){
-	nextPossibleScenes.resize(2);
-	nextPossibleScenes.at(0)=new takeFirstShot('t');
-	nextPossibleScenes.at(1)=new rejectFirstShot('r');
-	std::cout<<"Updated possible Scenes\n";
+    nextPossibleScenes.resize(2);
+    takeFirstShot* takeFirst = new takeFirstShot('t');
+    nextPossibleScenes.at(0)= takeFirst;
+    rejectFirstShot* rejectFirst = new rejectFirstShot('r');
+    nextPossibleScenes.at(1)= rejectFirst;
+    cout<<"Updated possible Scenes\n";
 }
+
 
 //take first shot
 void secondShotOptionScene::dialogue(){
-    string output = "";
-    output = "Kevin: WOOOAAAAAHH THAS LIQA!!!
-    \n Lilith: I'm tired of babysitting him, Chad, tag in.
-    \n Lilith grabs my arm and pulls me to the couch.
-    \n Lilith: So what do you think of Kevin , he's cute huh? 
-    \n Well yes..
-    \n Natalie: Dude…
-    \n Lilith: My bad I'm just trying to help my bro out. After all, I heard that you and your man just broke up..
-    \n She pulls out small bottle of tequila from her purse.
-    \n Lilith: Oops did that slip out , well I'm too deep in now to not tell you, but i need another shot so WE forget that I told you!
-    \n Take the shot: Kevin’s cute, and I wanna know more[t]
-    \n Reject the shot: I need to find my friend[r]";
+    string output = "Kevin: WOOOAAAAAHH THAS LIQA!!!" +
+    "\n Lilith: I'm tired of babysitting him, Chad, tag in."+
+    "\n Lilith grabs my arm and pulls me to the couch."+
+    "\n Lilith: So what do you think of Kevin , he's cute huh?"+
+    "\n Well yes..."+
+    "\n Natalie: Dude..."+
+    "\n Lilith: My bad I'm just trying to help my bro out. After all, I heard that you and your man just broke up..."+
+    "\n She pulls out small bottle of tequila from her purse."+
+    "\n Lilith: Oops did that slip out , well I'm too deep in now to not tell you, but i need another shot so WE forget that I told you!"+
+    "\n Take the shot: Kevin is cute, and I wanna know more[t]"+
+    "\n Reject the shot: I need to find my friend[r]";
     trickleDisplayString(output, 50);
 }
 void secondShotOptionScene::updatePossibleScenes(vector<Scene*>& nextPossibleScenes){
-	nextPossibleScenes.resize(2);
-	nextPossibleScenes.at(0)=new takeSecondShot('t');
-	nextPossibleScenes.at(1)=new rejectSecondShot('r');
-	std::cout<<"Updated possible Scenes\n";
+    nextPossibleScenes.resize(2);
+    takeSecondShot* takeShotTwo = new takeSecondShot('t');
+    nextPossibleScenes.at(0)= takeShotTwo;
+    rejwctSecondShot* noShotTwo = new rejectSecondShot('r')
+    nextPossibleScenes.at(1)= noShotTwo;
+    cout << "Updated possible Scenes\n";
 }
 
 
-//reject first shot 
+
+
+//reject first shot
 void rejectFirstShot::dialogue(){
-    string output = "";
-    output = "Chad: I know you're paranoid about your friend and all , but she aint here little lady. Stop killing the vibe.
-    \n Natalie - Fine I'll find her myself, jerk.";
+    string output = "Chad: I know you're paranoid about your friend and all , but she aint here little lady. Stop killing the vibe."+
+    "\n Natalie - Fine I'll find her myself, jerk.";
     trickleDisplayString(output, 50);
 }
 
 
+
+
 //takes second shot
+
+
 
 
 void Scene::trickleDisplayString(const string& inputString, uint8_t delay){
@@ -141,13 +159,15 @@ void Scene::trickleDisplayString(const string& inputString, uint8_t delay){
     const size_t PADDING = 2;
     const size_t MAX_LINE = BOX_WIDTH - PADDING * 2;
 
-    std::istringstream inputStream(inputString);
-    std::vector<string> lines;
+
+    istringstream inputStream(inputString);
+    vector<string> lines;
     string rawLine;
 
+
     // Word-wrap logic
-    while (std::getline(inputStream, rawLine)) {
-        std::istringstream wordStream(rawLine);
+    while (getline(inputStream, rawLine)) {
+        istringstream wordStream(rawLine);
         string word, line;
         while (wordStream >> word) {
             if (line.length() + word.length() + 1 > MAX_LINE) {
@@ -164,13 +184,16 @@ void Scene::trickleDisplayString(const string& inputString, uint8_t delay){
         if (rawLine.empty()) lines.push_back("");
     }
 
+
     // Box characters
     const char CORNER = '+';
     const char HORIZONTAL = '=';
     const char VERTICAL = '|';
 
+
     // Top border
     cout << CORNER << string(BOX_WIDTH, HORIZONTAL) << CORNER << endl;
+
 
     // Display lines with borders and padding
     for (const auto& l : lines) {
@@ -184,6 +207,8 @@ void Scene::trickleDisplayString(const string& inputString, uint8_t delay){
         cout << VERTICAL << endl;
     }
 
+
     // Bottom border
     cout << CORNER << string(BOX_WIDTH, HORIZONTAL) << CORNER << endl;
 }
+
